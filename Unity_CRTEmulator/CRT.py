f1c0b5f4909124da02953b3b688ba80bbaaccc1c
Rami-Pastrami <rami@ramipastrami.engineering>
2023-05-18 22:45:58 -0400

    Starting fresh with a different approach

--- Unity_CRTEmulator/CRT.py
+++ Unity_CRTEmulator/CRT.py
@@ -1,43 +1,30 @@
 import numpy as np
-from enum import Enum
 
 class CustomerRenderTexture():
 
-    # enums for dropdowns
-    Dim = Enum('Dimension', ['TwoD', 'TwoDArray', 'Cube', 'ThreeD'])  # Can't start with numbers lol, UNIMPLEMENTED
-    AA = Enum('Enum', ['NoSamples', 'TwoSamples', 'FourSamples', 'EightSamples'])  # Can't use None, UNIMPLEMENTED
-    Wrap = Enum('Wrap', ['Repeat', 'Clamp', 'Mirror', 'MirrorOnce', 'PerAxis'])
-    SSM = Enum('SSM', ['None', 'CompareDepths'])  # UNIMPLEMENTED
-    DSF = Enum('DSF', ['R8_UInt'])  # Only one that works
+    _data: np.ndarray
+    _data_dBuffered: np.ndarray
+    _xSize: float
+    _ySize: float
+    _isNormalized: bool
+    _numChannels: int
+    _numType: np.dtype
 
+    def __init__(self, xDim: int, yDim: int, numColorChannels: int, numType=np.single, isBuffered=True,
+                 isUpdateZoneNormalized=False):
+        '''
+        Initializes the eCRT (2D only for time being)
+        :param xDim: X size of the eCRT
+        :param yDim: Y size of the eCRT
+        :param numColorChannels: number of (color channels), in inclusive range 1-4
+        :param numType: See https://numpy.org/doc/stable/user/basics.types.html
+        :param isBuffered: Are we double buffering the CRT?
+        :param isUpdateZoneNormalized: is Update zone normalized instead of by pixel?
+        '''
 
-
-
-    '''
-    CF = Enum('CF', ['ARGB32', 'Depth', 'ARGBHalf', 'Shadowmap', 'RGB565', 'ARGB4444', 'ARGB1555', 'ARGB2101010',
-                     'ARGB64', 'ARGBFloat', 'RGFloat', 'RGHalf', 'RFloat', 'RHalf', 'R8', 'ARGBInt', 'RGInt', 'RInt',
-                     'BGRA32', 'RGB111110Float', 'RG32', 'RGBAUShort', 'RG16', 'BGRA10101010_XR', 'BGR101010_XR', 'R16'])
-    '''
-
-    @property
-    def Dimension(self):
-        return self._Dimension
-    @Dimension.setter
-    def Dimension(self, x):
-        self._Dimension = x
-
-
-
-
-
-    # Private vars
-
-    _Dimension: Dim = Dim.TwoD
-    _Size: np.ndarray = np.array([1, 1]).astype(int)
-    _ColorFormat: CF = CF.R8
-    # The following are unused
-    _AntiAliasing: AA = AA.NoSamples
-
-
+        self._xSize = float(xDim); self._ySize = float(yDim)
+        self._numChannels = numColorChannels
+        self._isNormalized = isUpdateZoneNormalized
+        self._numType = numType
 
 
